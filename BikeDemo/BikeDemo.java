d552fa779b10fdae8996202f7b1b29be334e79fb
Alden <aldendzakwan@gmail.com>
2026-08-28 08:32:11 +0700

    Percobaan2__Jobseet1

--- BikeDemo/BikeDemo.java
+++ BikeDemo/BikeDemo.java
@@ -4,6 +4,7 @@ public class BikeDemo {
     public static void main(String[] args) {
         Bike mountainBikel = new Bike();
         Bike mountainBike2 = new Bike();
+        RoadBike roadBike1 = new RoadBike();
 
         mountainBikel.setBrand("Trek");
         mountainBikel.speedAcceleration(10);
@@ -14,5 +15,11 @@ public class BikeDemo {
         mountainBike2.speedAcceleration(20);
         mountainBike2.gearChanges(3);
         mountainBike2.printInfo();
+
+        roadBike1.setBrand("Specialized");
+        roadBike1.setTireWidth(25);
+        roadBike1.speedAcceleration(15);
+        roadBike1.gearChanges(4);
+        roadBike1.printInfo();
     }
 }
