fbc0e74090b4c1b122930c91b31ae9df87a7aa92
Alden <aldendzakwan@gmail.com>
2026-09-04 08:53:19 +0700

    Langkah3_Jobsheet2

--- Jobsheet2/Rectangle.java
+++ Jobsheet2/Rectangle.java
@@ -3,4 +3,12 @@ package Jobsheet2;
 public class Rectangle {
     int width;
     int height;
+
+    int area(){
+        return width * height;
+    }
+
+    int perimeter(){
+        return 2 * (width + height);
+    }
 }
