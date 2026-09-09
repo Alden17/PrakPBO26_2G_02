3d12f975ab489ed12112bbaad6ee7d716c8e6569
Alden <aldendzakwan@gmail.com>
2026-09-09 11:03:34 +0700

    Tugas_mandiri_Jobsheet2

--- Jobsheet2/Main.java
+++ Jobsheet2/Main.java
@@ -1,17 +1,12 @@
 package Jobsheet2;
 
 public class Main {
-    public static void main(String[] args) {
-        Rectangle[] shapes = new Rectangle[3];
-        shapes[0] = new Rectangle(6, 4);
-        shapes[1] = new Rectangle(3, 3);
-        shapes[2] = new Rectangle(8, 2);
+    public static void main(String[] args) {    
+        Circle c = new Circle (5);
+        int radius = 5;
 
-        for (Rectangle r : shapes) {
-            System.out.println("Area: " + r.area() + ", Perimeter: " + r.perimeter() );
-        }
-
-        Student s = new Student("Nadia", "S001", 3.8 );
-        System.out.println(s.describe());
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + c.area());
+        System.out.println("Keliling: " + c.circumference());
     }
 }
