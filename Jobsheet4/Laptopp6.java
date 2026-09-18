c58f6038d58da321d9142d4576ff95ad73070009
Alden <aldendzakwan@gmail.com>
2026-09-18 09:46:12 +0700

    Percobaan6_Jobsheet4

---
+++ Jobsheet4/Laptopp6.java
@@ -0,0 +1,14 @@
+package Jobsheet4;
+
+public class Laptopp6 {
+    private String merk;
+
+    public Laptopp6(String merk) {
+        this.merk = merk;
+    }
+
+    public void cetakDokumen(Printer printer, String namaFile) {
+        System.out.println(merk + " mengirim dokumen ke printer...");
+        printer.cetak(namaFile);
+    }
+}
